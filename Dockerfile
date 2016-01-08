34976899f4d8b1b4af1d347716bb2f96a3a60507
xrow <bjoern@xrow.de>
2016-01-08 04:27:07 +0100

    - add settings

--- Dockerfile
+++ Dockerfile
@@ -10,15 +10,17 @@ ENV SOLR_VERSION=4.10.4
 ENV SOLR=solr-$SOLR_VERSION
 ENV SOLR_USER=solr
 
+COPY ezp-default/ /opt/solr/example/solr/ezp-default/
+COPY lib/ /opt/solr/example/solr/lib/
+COPY solr.xml /opt/solr/example/solr/solr.xml
+COPY patch.sh patch.sh
+RUN sh patch.sh
+
 # Bug https://github.com/docker-solr/docker-solr/commit/949d6bece4e2ae1189d84210ae0b54b7ba87a37c
 # 5.4 Fix
 RUN sed -i -e 's/#SOLR_PORT=8983/SOLR_PORT=8983/' /opt/solr/bin/solr.in.sh
 # 4.10 Fix
 RUN echo "SOLR_PORT=8983" >> /opt/solr/bin/solr.in.sh
-
-COPY ezp-default/ /opt/solr/example/solr/ezp-default/
-COPY lib/ /opt/solr/example/solr/lib/
-COPY solr.xml /opt/solr/example/solr/solr.xml
 
 USER root
 RUN chown -R $SOLR_USER:$SOLR_USER /opt/solr /opt/$SOLR
---
+++ patch.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+SOLR_CONFIG=/opt/solr/example/solr/ezp-default/conf/solrconfig.xml
+SOLR_SCHEMA=/opt/solr/example/solr/ezp-default/conf/schema.xml
+sed -i '/type="text_icu"/d' ${SOLR_SCHEMA}
+sed -i '/<fieldType name="text_icu" class="solr.TextField" positionIncrementGap="100" autoGeneratePhraseQueries="false">/,/<\/fieldType>/d' ${SOLR_SCHEMA}
+sed -i 's@<str name=\"confFiles\">.*@<str name=\"confFiles\">schema.xml,elevate.xml,stopwords.txt</str>@g' ${SOLR_CONFIG}
+sed -i 's@<useColdSearcher>.*@<useColdSearcher>true</useColdSearcher>@g' ${SOLR_CONFIG}
+sed -i 's@<maxWarmingSearchers>.*@<maxWarmingSearchers>1</maxWarmingSearchers>@g' ${SOLR_CONFIG}
+sed -i 's@<filterCache class=\"solr.FastLRUCache\".*@<filterCache class=\"solr.FastLRUCache\" size=\"512\" initialSize=\"512\" autowarmCount=\"32\" />@g' ${SOLR_CONFIG}
+sed -i 's@<queryResultCache class=\"solr.LRUCache\".*@<queryResultCache class=\"solr.LRUCache\" size=\"512\" initialSize=\"512\" autowarmCount=\"32\" />@g' ${SOLR_CONFIG}
+sed -i '
+/<\/fields>/ i\
+<dynamicField name="*____loc" type="location" indexed="true" stored="true"\/>\
+<dynamicField name="*____loc_0_coordinate" type="double" indexed="true" stored="true"\/>\
+<dynamicField name="*____loc_1_coordinate" type="double" indexed="true" stored="true"\/>\
+' ${SOLR_SCHEMA}
+sed -i '
+/<\/schema>/ i\
+     <copyField source="*_lk" dest="ezf_df_tags"\/>\
+     <copyField source="*_k" dest="ezf_df_tags"\/>\
+' ${SOLR_SCHEMA}
+sed -i '
+/<\/fields>/ i\
+     <field name="ezf_df_tags" type="text" indexed="true" stored="true" multiValued="true" termVectors="true"\/>\
+' ${SOLR_SCHEMA}
+
+#sed -i 's@solr.log=.*@solr.log=/var/log/@g' $RPM_BUILD_ROOT%{_datadir}/ezfind/resources/log4j.properties
